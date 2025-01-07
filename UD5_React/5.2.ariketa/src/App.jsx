3a1a155c7be576485faf05134d47eb2e5a359631
Ivan Hernandez <ihernandezgo23wg@ikzubirimanteo.com>
2025-01-07 13:57:08 +0100

    Ariketa hasita 5.2

---
+++ UD5_React/5.2.ariketa/src/App.jsx
@@ -0,0 +1,12 @@
+import { Header } from "./Components/Header";
+
+const App = () => {
+  return (
+    <main className="px-4 pt-6 pb-3 font-Inter lg:px-40 lg:pt-20">
+      <Header />
+      <div className="md:flex md:gap-8 md:mb-10"></div>
+    </main>
+  );
+};
+
+export default App;
